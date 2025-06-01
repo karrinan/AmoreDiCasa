7c9d4ebbd1a0eec0b8e64c0f9f2c9ab95b1bb399
Karina N <160251085+karrinan@users.noreply.github.com>
2025-06-01 22:01:34 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,7 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 // Получаем данные из POST-запроса
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
